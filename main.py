288119bcef741a3fd28d37f0e877d99ff02ff66a
OmriYahav <122606148+OmriYahav@users.noreply.github.com>
2023-03-05 22:36:08 +0200

    requestTest1.2

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 import requests
 
 IpApi = requests.get('http://ip-api.com/json/')
-weather = requests.get('http://wttr.in')
-cryptoRate = requests.get('http://rate.sx')
-Ipconfig = requests.get('http://ifconfig.io/ip')
-
 IP = IpApi.json()
-print(IpApi.text)
-print(weather.text)
-print(cryptoRate.text)
-print(Ipconfig.text)
+for i, value in IP.items():
+    print(f"\t{i.upper()}: {value}")
 
-
-# print(IP)
-# print("Query:",IP['status'])
-# print("IP:",IP['query'])
-# print("ISP:",IP['as'])
-# print("AS:",IP['isp'])
-# print("Country:",IP['country'])
-# print("Region Name:",IP['regionName'])
-# print("City:",IP['city'])
-# print("Latitude:",IP['lat'])
-# print("Longtitude:",IP['lon'])
-# print("Country:",IP['country'])
