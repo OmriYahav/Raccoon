22397aed13929d98d6a6251afad4ca17d84c8362
OmriYahav <122606148+OmriYahav@users.noreply.github.com>
2023-03-06 09:18:21 +0200

    requestTest1.3

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import requests
 IpApi = requests.get('http://ip-api.com/json/')
 IP = IpApi.json()
 for i, value in IP.items():
-    print(f"\t{i.upper()}: {value}")
+     print(f"\t{i.upper()}: {value}")
 
